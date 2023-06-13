33b9f54f0c8317bdf5c25a0542387cd15fce1d22
paul14930 <paul.pelerins@syndicompare.com>
2023-06-13 16:28:08 +0200

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :tasks
+  get "tasks", to: "task#index"
+  get "tasks/new", to: "task#new", as: :new_task
+  post "tasks", to: "task#create"
+  get "tasks/:id", to: "task#show", as: :task
+  get "tasks/:id/edit", to: "task#edit", as: :edit_task
+  patch "tasks/:id/", to: "task#update"
+  get "tasks/:id/destroy", to: "task#destroy", as: :destroy_task
 end
